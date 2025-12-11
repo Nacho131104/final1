ff9eee0105f784205e1690a431870d3ca379a940
Nacho <nacholopezamat@gmail.com>
2025-12-11 11:25:54 -0500

    mongod

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hola mundo")
+import dotenv from "dotenv"
+import { ApolloServer } from "apollo-server";
+import { connectmongodb } from "./mongo/conexion"
+
+dotenv.config()
+
+
+const app = async () => {
+  await connectmongodb();
+};
+
+
+
+app().catch(err=>console.error(err));
